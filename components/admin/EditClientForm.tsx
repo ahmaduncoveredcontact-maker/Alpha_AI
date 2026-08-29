48f1dc52e721b5b6eedf0d0481acd38cf00e2333
Ikhlaq <ikhlaqahmad881@gmail.com>
2026-08-29 18:31:12 +0500

    Add live QR preview on admin edit page and polish design2

--- components/admin/EditClientForm.tsx
+++ components/admin/EditClientForm.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
-import QRCode from 'qrcode.react';
+import { QRCode } from 'qrcode.react';
 import QRDisplay from '@/components/client/QRDisplay';
 import WebhookUrlDisplay from '@/components/client/WebhookUrlDisplay';
 
